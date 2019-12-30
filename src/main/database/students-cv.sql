f06886eb7297204951613be014f08f2ad8a5e8d5
AndreasPatsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-12-30 23:40:12 +0200

    Update students-cv.sql

--- src/main/database/students-cv.sql
+++ src/main/database/students-cv.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `students_cv` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `students_cv` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `students_cv`;
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
 -- Host: localhost    Database: students_cv
 -- ------------------------------------------------------
--- Server version	8.0.16
+-- Server version	8.0.13
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -37,7 +37,7 @@ CREATE TABLE `authorities` (
 
 LOCK TABLES `authorities` WRITE;
 /*!40000 ALTER TABLE `authorities` DISABLE KEYS */;
-INSERT INTO `authorities` VALUES (1,'ROLE_USER'),(2,'ROLE_ADMIN');
+INSERT INTO `authorities` VALUES (1,'ROLE_STUDENT'),(2,'ROLE_COMPANY'),(3,'ROLE_ADMIN');
 /*!40000 ALTER TABLE `authorities` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,12 +53,13 @@ CREATE TABLE `companies` (
   `email` varchar(100) NOT NULL,
   `company_name` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `units` bigint(20) DEFAULT NULL,
+  `logo_path` varchar(250) DEFAULT NULL,
   `user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `usid_idx` (`user_id`),
   CONSTRAINT `usid` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +68,7 @@ CREATE TABLE `companies` (
 
 LOCK TABLES `companies` WRITE;
 /*!40000 ALTER TABLE `companies` DISABLE KEYS */;
+INSERT INTO `companies` VALUES (7,'tasos@icap.gr','ICAP AE',NULL,NULL,47),(8,'tasos@agile.gr','Agile AE',NULL,NULL,49);
 /*!40000 ALTER TABLE `companies` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,11 +86,11 @@ CREATE TABLE `cvs` (
   `file_type` varchar(45) NOT NULL,
   `size` bigint(20) NOT NULL,
   `time_insert` datetime NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
+  `student_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `uid_idx` (`user_id`),
-  CONSTRAINT `uid` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `stid_idx` (`student_id`),
+  CONSTRAINT `stid` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,6 +99,7 @@ CREATE TABLE `cvs` (
 
 LOCK TABLES `cvs` WRITE;
 /*!40000 ALTER TABLE `cvs` DISABLE KEYS */;
+INSERT INTO `cvs` VALUES (21,'erg.txt','http://172.16.3.157:8888/downloadFile/erg.txt','multipart/form-data',31,'2019-12-30 14:51:29',24);
 /*!40000 ALTER TABLE `cvs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,7 +126,7 @@ CREATE TABLE `students` (
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `userId_idx` (`user_id`),
   CONSTRAINT `userId` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +135,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'sotirinio@hotmail.com','Sotiris','Patsimas',10,1,'1997-05-08',NULL,6970166692,NULL,2);
+INSERT INTO `students` VALUES (24,'sotirinio@hotmail.com','Sotiris','Patsimas',3,1,'1997-05-03','I am very good at football!!!',6986803782,'sotiris.jpg',48),(25,'billysdnlew@hotmail.com','Vasilis','Kamarados',2,1,'2019-12-04',NULL,6988762723,NULL,50);
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,7 +154,7 @@ CREATE TABLE `users` (
   `time_insert` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -160,7 +163,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'andreas','$2a$10$3.9Z9O3gh6G5srOBdZX2X.y7p8YJwEPVg30SeGtTbNeyDiJ3CxXv2',1,'2019-03-16 15:09:17'),(2,'sotiris','$2a$10$tnIAFEF1TdlAmLHDT86HXeZiYv7MqBlylaQpOwsceiGS/kMgmXLtS',1,'2019-03-16 15:09:17');
+INSERT INTO `users` VALUES (1,'andreas','$2a$10$V59heakpW8TMFdfxYEkpa.niyJRGHhSLbXIF.or9rVOLq.Irze8zO',1,'2019-03-16 15:09:17'),(47,'icap','$2a$10$baIbigmX9WA8RLDzRhXTJOd8clSmCQr90.OC3gkvQW1XKWWE5hwuS',1,'2019-12-30 14:33:30'),(48,'sotiris','$2a$10$cGFDlSZMZl/5leTgVqAuAeLqBAgNQiFd6I0qGBiJNLfyB0sqZbmdK',1,'2019-12-30 14:33:31'),(49,'agile','$2a$10$f5oNaITmSuzYLokyFGiwPOaLEqtkFd0.imvk4H6kfR8Gro/dtPYN6',1,'2019-12-30 15:44:45'),(50,'billy','$2a$10$VxWn9PUVqL6fsXSYZv2tQu.x70KTROl4oSUuNSI0FId1aqRxyVxsy',1,'2019-12-30 21:36:06');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,13 +190,9 @@ CREATE TABLE `users_authorities` (
 
 LOCK TABLES `users_authorities` WRITE;
 /*!40000 ALTER TABLE `users_authorities` DISABLE KEYS */;
-INSERT INTO `users_authorities` VALUES (2,1),(1,2);
+INSERT INTO `users_authorities` VALUES (48,1),(50,1),(47,2),(49,2),(1,3);
 /*!40000 ALTER TABLE `users_authorities` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'students_cv'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -204,4 +203,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-13 16:30:51
+-- Dump completed on 2019-12-30 23:39:33
