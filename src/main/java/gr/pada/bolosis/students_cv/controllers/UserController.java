eef77250ed8f12f5e3beb56add401fdbe7f08795
AndreasPatsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2020-01-02 17:50:03 +0200

    updates

--- src/main/java/gr/pada/bolosis/students_cv/controllers/UserController.java
+++ src/main/java/gr/pada/bolosis/students_cv/controllers/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
-@CrossOrigin("http://127.0.0.1:5501")
+@CrossOrigin("*")
 @RestController
 @Slf4j
 @RequestMapping(value = "/user")
